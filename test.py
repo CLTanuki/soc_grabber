70273d68477b9d2bead2e15f0ced7aed6f2aed78
CLTanuki <cltanuki@gmail.com>
2014-09-01 16:13:47 +0400

    In Progress

--- test.py
+++ test.py
@@ -1,42 +1,38 @@
 __author__ = 'cltanuki'
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Process, Queue
 
 
-domain = 'https://twitter.com'
+class Grabber():
+
+    def __init__(self, domain, start_url):
+        self.domain = domain
+        self.start_url = start_url
+        self.dir_links = Queue()
+        self.profile_links = Queue()
+
+    def get_start_links(self):
+        page = requests.get(self.start_url)
+        soup = BeautifulSoup(page.text)
+        links_div = soup.find('div', class_='top-levels')
+        links = [self.domain + link['href'] for link in links_div.find_all('a')]
+        links.append(self.domain + '/i/directory/profiles/a')
+        return links
+
+    def grab_links(self, link):
+        page = requests.get(link)
+        soup = BeautifulSoup(page.text)
+        links_div = soup.find('div', class_='directory-page').find('div', class_='row')
+        links = [self.domain + link['href'] for link in links_div.find_all('a')]
+
+    def main(self):
+        self.grab_links(self.get_start_links())
 
 
-def get_links():
-    page = requests.get('https://twitter.com/i/directory/profiles')
-    soup = BeautifulSoup(page.text)
-    links_div = soup.find('div', class_='top-levels')
-    links = [domain + link['href'] for link in links_div.find_all('a')]
-    links.append(domain + '/i/directory/profiles/a')
-    return links
+if __name__ == '__main__':
+    grabber = Grabber(domain='https://twitter.com', start_url='https://twitter.com/i/directory/profiles')
+    grabber.main()
 
-
-def get_profile_links(links):
-    level = 3
-    last_level = 4
-    dir_links = {}
-    dir_links[last_level] = links
-    print(dir_links)
-    profile_links = {}
-    while level != 0:
-        dir_links[level] = []
-        links = dir_links.get(last_level)
-        print('links: ', links)
-        for link in links:
-            print('link: ', link)
-            page = requests.get(link)
-            soup = BeautifulSoup(page.text)
-            links_div = soup.find('div', class_='directory-page').find('div', class_='row')
-            links = [domain + link['href'] for link in links_div.find_all('a')]
-            dir_links[level] += links
-        level -= 1
-        last_level -= 1
-    print(type(dir_links))
-
-get_profile_links(get_links())
 # for link in links:
 #     print(link)
