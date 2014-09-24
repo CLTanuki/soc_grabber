13d6ef134b86cc66a92b30bf05532f6d24ca17f6
CLTanuki <cltanuki@gmail.com>
2014-09-24 16:44:32 +0400

    Added: VkUserInfo table.

--- db_work/models.py
+++ db_work/models.py
@@ -1,13 +1,11 @@
 __author__ = 'cltanuki'
 from peewee import *
 
-soc_db = PostgresqlDatabase('soc', user='soc', password='default', host='37.143.13.95')
-
-# soc_db = SqliteDatabase('soc.sqlite')
+soc_db = PostgresqlDatabase(database='soc', user='soc', password='default', host='37.143.13.95')
 
 
 class Proxy(Model):
-    body = CharField(unique=True, index=True)
+    body = CharField(max_length=15, unique=True, index=True)
     level = IntegerField(null=True, default=True)
 
     class Meta:
@@ -15,7 +13,7 @@ class Proxy(Model):
 
 
 class TwitterUser(Model):
-    url = CharField(unique=True, index=True)
+    id_str = CharField(unique=True, index=True)
     is_ru = BooleanField(null=True)
 
     class Meta:
@@ -23,8 +21,33 @@ class TwitterUser(Model):
 
 
 class VkUser(Model):
-    url = CharField(unique=True, index=True)
+    id_str = BigIntegerField(unique=True, index=True)
     is_ru = BooleanField(null=True)
+    deactivated = BooleanField(null=True)
+
+    class Meta:
+        database = soc_db
+
+
+class VkUserInfo(Model):
+    user = ForeignKeyField(rel_model=VkUser)
+    screen_name = CharField(null=True)
+    photo_50 = CharField(null=True)
+    profile_fill_count = IntegerField(null=True)
+    country = IntegerField(null=True)
+    bdate = CharField(max_length=10, null=True)
+    last_seen = CharField()
+    added_media_count = IntegerField()
+    followers_count = IntegerField()
+    has_mobile = BooleanField()
+    post_count = IntegerField(null=True)
+    friends_count = IntegerField(null=True)
+
+    class Meta:
+        database = soc_db
+
+
+class VkPosts:
 
     class Meta:
         database = soc_db
